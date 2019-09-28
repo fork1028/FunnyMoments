985a086035fd513d5a2d2f24725ca3f5483655ef
fork1028 <xichen_@live.cn>
2019-09-28 17:57:38 -0400

    update

--- FunnyMoments/ViewController.swift
+++ FunnyMoments/ViewController.swift
@@ -13,14 +13,22 @@ class ViewController: UIViewController{
     
     @IBOutlet weak var videoView: UIImageView!
     
+    
     var videoPicker=UIImagePickerController()
+    var videoURL: NSURL?
+    
     
     @IBAction func importBtnTapped(_ sender: Any) {
-        videoPicker.sourceType = .photoLibrary
         videoPicker.allowsEditing = false
+        videoPicker.sourceType = .photoLibrary
+        videoPicker.delegate = self
+        videoPicker.mediaTypes = ["public.image", "public.movie"]
+
         present(videoPicker, animated: true, completion: nil)
         
     }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,13 +39,20 @@ class ViewController: UIViewController{
 }
 
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        <#code#>
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+
+        // Set photoImageView to display the selected image.
+        videoView.image = selectedImage
+        
+        videoURL = info[UIImagePickerController.InfoKey.mediaURL] as? NSURL
+        print(videoURL)
+
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+        
     }
 }
-
-
-
-
-
-
